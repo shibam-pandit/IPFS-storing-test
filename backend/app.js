404fb98f570e945874cb8b90204fe3942d4dfea1
Shibam Pandit <shibampandit31@gmail.com>
2025-01-31 21:39:06 +0530

    some changes

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,3 @@
-// require('dotenv').config();
-// const express = require('express');
-// const cors = require('cors');
-// const ipfsRoutes = require('./routes/ipfsRoutes');
-// const blockchainRoutes = require('./routes/blockchainRoutes');
-
-// const app = express();
-
-// // Enable CORS for all routes
-// app.use(cors({
-//     origin: "http://localhost:5173", // Allow requests from React frontend
-//     methods: "GET,POST,PUT,DELETE",
-//     allowedHeaders: "Content-Type,Authorization",
-//   }));
-
-// app.use(express.json());
-
-// // Routes
-// app.use('/api/ipfs', ipfsRoutes);
-// app.use('/api/blockchain', blockchainRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
-
-
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,3 @@
-// // src/App.jsx
-// import React, { useState } from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import ConnectWallet from './components/connectWallet.jsx';
-// import Home from './pages/Home';
-// import UploadFolder from './pages/UploadFolder.jsx';
-// import UserFolders from './pages/UserFolders.jsx';
-
-// const App = () => {
-//   const [account, setAccount] = useState(null);
-
-//   return (
-//     <Router>
-//       <div>
-//         <h1>Web3 IPFS Storage</h1>
-//         {!account ? (
-//           <ConnectWallet setAccount={setAccount} />
-//         ) : (
-//           <div>
-//             <h2>Welcome, {account}</h2>
-//             <Routes>
-//               <Route path="/" element={<Home />} />
-//               <Route path="/upload" element={<UploadFolder account={account} />} />
-//               <Route path="/userfolders" element={<UserFolders account={account} />} />
-//             </Routes>
-//           </div>
-//         )}
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
